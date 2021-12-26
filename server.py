fbef5958c705c25fbbf56c3f46aaf549228fd080
KeithLion <nortonkiara6@gmail.com>
2021-12-25 18:30:30 -0800

    added the routes to the server

---
+++ server.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, redirect, session, request
+from survey import Survey
+app = Flask(__name__)
+app.secret_key = 'lock of the key'
+
+
+@app.route('/')
+def survey():
+    return render_template('survey.html')
+
+
+@app.route('/pass_result', methods=['post'])
+def pass_result():
+    if not Survey.survey(request.form):
+        return redirect('/')
+
+    session['name'] = request.form['name']
+    session['location'] = request.form['location']
+    session['language'] = request.form['language']
+    session['comment'] = request.form['comment']
+    return redirect('/results')
+
+
+@app.route('/results')
+def result():
+    return render_template('result_page.html')
+
+
+@app.route('/return')
+def back():
+    return redirect('/')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
